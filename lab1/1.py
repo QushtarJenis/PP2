e9abc9f63e0dd931be1d33597f1629182991e519
Qushtar <jengisqushtar@gmail.com>
2025-06-03 10:53:12 +0500

    lab1

---
+++ lab1/1.py
@@ -0,0 +1,106 @@
+# import sys
+
+# print(sys.version)
+
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+# x = 5
+# y = "Hello, World!"
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+# x = 4  # x is of type int
+# print(x)
+# x = "Sally"  # x is now of type str
+# print(x)
+
+# x = str(3)  # x will be '3'
+# y = int(3)  # y will be 3
+# z = float(3)  # z will be 3.0
+# print(x)
+# print(y)
+# print(z)
+# print(x == y)
+# print(y == z)
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+# a = 4
+# A = "Sally"
+# print(a)
+# print(A)
+
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
+# print(myvar)
+# print(my_var)
+# print(_my_var)
+# print(myVar)
+# print(MYVAR)
+# print(myvar2)
+
+# false = False
+# print(false)
+# # False = 'asdf'
+# # print(False)
+# true = True
+# print(true)
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# x, y, z = "Orange", 222, False
+# print(x)
+# print(y)
+# print(z)
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# # fruits = ["apple", "banana", "cherry", "sdf"]
+# # fruits = ["apple", "banana"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+
+# x = 5
+# y = 10
+# print(x + y)
+
+# # x = 5
+# # y = "10"
+# # print(x + y)
+
+# x = 5
+# y = "John"
+# print(x, y)
+# print(type(x), type(y))
+# typeX = type(x)
+# print(type(typeX))
---
+++ lab1/3.py
@@ -0,0 +1,61 @@
+# import random
+
+# for i in range(5):
+#     print(random.randrange(1, 10))
+
+# print("It's alright")
+# print("He is called 'Johnny'")
+# print('He is called "Johnny"')
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# a = "Hello, World!"
+# print(a[1])
+
+# for x in "banana":
+#     print(x)
+
+# a = "Hello, World!"
+# print(len(a))
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+# b = "Hello, World!"
+# print(b[2:5])
+# print(b[:5])
+# print(b[2:])
+# print(b[-5:-2])
+
+# a = "Hello, World!"
+# print(a.upper())
+# print(a.lower())
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
+# print(a.replace("h", "j"))
+
+# age = 36
+# txt = f"My name is John, I am {age}"
+# print(txt)
+
+# price = 59
+# txt = f"The price is {price:.2f} dollars"
+# print(txt)
+
+# txt = 'We are the so-called "Vikings" from the north.'
+# print(txt)
+
+# txt = "Hello \bWorld!"
+# print(txt)
+
+txt = "\110\145\154\154\157"
+print(txt)
+
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
+print(txt.encode())
